2b0ca6a48f4c4d24d6779ed501429320a2da5bfc
Fredy-SSA <alin@zizo.ro>
2020-04-10 14:51:31 +0300

    ps

--- PowerShell/Help.ps1
+++ PowerShell/Help.ps1
@@ -1,3 +1,36 @@
 #Count cmdlets
 Get-Command | Measure-Object
 
+#
+Show-Command 
+
+#get info 
+Get-Command *-Disk
+Get-Command *-variable
+
+get-help Get-Disk -Examples
+help Get-Disk -Examples
+man Get-Disk -Examples
+
+help  Get-Alias -Examples
+help  Get-Alias -Online
+
+help  Get-Alias -ShowWindow 
+help  Get-Alias -Full
+
+
+get-help Get-EventLog -ShowWindow 
+
+
+Get-Alias -Definition Get-Service
+Get-Alias -Definition Get-ChildItem
+
+<#
+
+dir
+Get-ChildItem
+ls
+
+#>
+
+#find share cmd
