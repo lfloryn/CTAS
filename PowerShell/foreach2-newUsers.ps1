da2ba5c9f7313d41867893cd43f36458f889901c
Fredy-SSA <alin@zizo.ro>
2020-04-10 15:41:25 +0300

    ps

---
+++ PowerShell/foreach2-newUsers.ps1
@@ -0,0 +1,15 @@
+﻿$Password = Read-Host -AsSecureString
+
+foreach ($item in $lista)
+{
+##
+#"--------------------------------------------------"
+ #Write-host " Numele este"  $item.nume -BackgroundColor Cyan -ForegroundColor DarkGreen
+
+# Write-host " Prenumele este"  $item.prenume -BackgroundColor Red -ForegroundColor Yellow
+
+New-LocalUser $item.nume -Password $Password -FullName "$($item.nume) $($item.prenume)" -Description "Contul lui $($item.nume) $($item.prenume)" 
+
+
+}
+
