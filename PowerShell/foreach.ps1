57aed5a103112ee17f39bd7250c43b5afaff9fc2
Fredy-SSA <alin@zizo.ro>
2020-04-10 14:30:04 +0300

    aa

---
+++ PowerShell/foreach.ps1
@@ -0,0 +1,13 @@
+﻿$lista = import-csv ".\Powershell\useri.csv"
+
+foreach ($item in $lista)
+{
+
+"--------------------------------------------------"
+ Write-host " Numele este"  $item.nume -BackgroundColor Cyan -ForegroundColor DarkGreen
+
+ Write-host " Prenumele este"  $item.prenume -BackgroundColor Red -ForegroundColor Yellow
+
+}
+
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
